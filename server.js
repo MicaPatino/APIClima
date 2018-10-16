e106db393acd97fe6345d48631635a820dc74593
lau-vera <mlrv22@gmail.com>
2018-10-16 20:01:04 -0300

    proyecto completo

--- server.js
+++ server.js
@@ -10,17 +10,40 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
 
-
+var provinces = {};
 app.get ('/', function (req,res){
-  
+ 
   var url = 'https://apis.datos.gob.ar/georef/api/provincias';
   request(url, function(err,response, data){
-    var provincies = JSON.parse(data);
-    res.render('index', {weather: null, error: null, provincies: provincies});
+    provinces = JSON.parse(data);
+    res.render('index', {weather: null, error: null, provinces: provinces});
   })
 })
+app.post('/', function(req,res){
+  var location =JSON.parse(req.body.location);
+  var url ='http://api.openweathermap.org/data/2.5/weather?lat='+location.lat+'&lon='+location.lon+"&units=metric&appid="+apiKey;
+  request(url, function (err, response, data){
+    if(err){
+      res.render('index', {weather: null, error: err, provinces: provinces});
+    } else {
+      var weather = JSON.parse(data)
+      
 
-app.post('/', function (req, res) {
+      if(weather.cod !== 200){
+        res.render('index', {weather: null, error: 'Error, please try again', provinces:provinces});
+      } else {
+        var temp = weather.main.temp;
+        var name = weather.name;
+        var text = 'la temperatura en: '+name+' es: '+ temp;
+        res.render('index', {weather: text, error: null, provinces:provinces});
+      }
+    }
+  })
+})
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+/*app.post('/', function (req, res) {
   var city = req.body.city;
   var url = `https://api.openweathermap.org/data/2.5/find?q=${city}&units=metric&appid=${apiKey}`
   request(url, function (err, response, data) {
@@ -40,8 +63,6 @@ app.post('/', function (req, res) {
       }
     }
   });
-})
+})*/
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+
